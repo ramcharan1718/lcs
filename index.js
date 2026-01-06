06cc045e8c5de954345a240d2693e183827b44f0
ramcharan1718 <2310030336@klh.edu.in>
2026-01-06 14:11:56 +0530

    added longest common substring API

---
+++ index.js
@@ -0,0 +1,51 @@
+import express from 'express'
+
+const app = express()
+app.use(express.json())
+
+function longestCommonSubstring(s1, s2) {
+  let m = s1.length
+  let n = s2.length
+  let dp = Array(m + 1).fill(0).map(() => Array(n + 1).fill(0))
+  let maxLen = 0
+  let endIndex = 0
+
+  for (let i = 1; i <= m; i++) {
+    for (let j = 1; j <= n; j++) {
+      if (s1[i - 1] === s2[j - 1]) {
+        dp[i][j] = dp[i - 1][j - 1] + 1
+        if (dp[i][j] > maxLen) {
+          maxLen = dp[i][j]
+          endIndex = i
+        }
+      }
+    }
+  }
+
+  return s1.substring(endIndex - maxLen, endIndex)
+}
+
+app.get('/', (req, res) => {
+  res.send('Longest Common Substring API is running')
+})
+
+app.post('/lcs', (req, res) => {
+  const { string1, string2 } = req.body
+
+  if (!string1 || !string2) {
+    return res.status(400).json({
+      error: 'Both string1 and string2 are required'
+    })
+  }
+
+  const result = longestCommonSubstring(string1, string2)
+
+  res.json({
+    longestCommonSubstring: result,
+    length: result.length
+  })
+})
+
+app.listen(3000, () => {
+  console.log('Server running on http://localhost:3000')
+})
